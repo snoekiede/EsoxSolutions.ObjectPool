5723d3d6708a846d6a7bb620d90a283c6b4f7f21
Iede Snoek <info@esoxsolutions.nl>
2023-11-14 07:02:25 +0100

    Started on dynamic object pool

---
+++ EsoxSolutions.ObjectPool/Pools/DynamicObjectPool.cs
@@ -0,0 +1,31 @@
+﻿using EsoxSolutions.ObjectPool.Exceptions;
+using EsoxSolutions.ObjectPool.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsoxSolutions.ObjectPool.Pools
+{
+    /// <summary>
+    /// Queryable object pool
+    /// </summary>
+    /// <typeparam name="T">the type to be stored in the object pool</typeparam>
+    public class DynamicObjectPool<T> : QueryableObjectPool<T> where T : class, new()
+    {
+        /// <summary>
+        /// The constructor for the queryable object pool
+        /// </summary>
+        /// <param name="initialObjects">the initial objects</param>
+        public DynamicObjectPool(List<T> initialObjects) : base(initialObjects)
+        {
+        }
+
+        public DynamicObjectPool(Func<T> creationFunction) : base(new List<T>())
+        {
+        }
+
+        
+    }
+}
